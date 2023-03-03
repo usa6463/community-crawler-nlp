47960a20885e1282982add424ed9492507d03192
usa6463 <usa6463@gmail.com>
2023-03-03 20:21:22 +0900

    Update tag_morpheme.py
    - 스크립트 시작시 table drop 하는 로직 추가

--- tag_morpheme.py
+++ tag_morpheme.py
@@ -8,7 +8,7 @@ from elasticsearch_dsl7 import connections
 from konlpy.tag import Okt
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
+import logging
 from db_migration.models.morpheme_info import MorphemeInfo
 
 PGSQL_URL = os.getenv("PGSQL_URL")
@@ -28,24 +28,22 @@ s = Search().extra(track_total_hits=True).sort('dt').using(client).query(q)
 response = s.execute()
 hit_count = s.count()
 
-print(f"num: {s.count()}")
+logging.warning(f"post count: {s.count()}")
 okt = Okt()
 
-# we can now construct a Session() and include begin()/commit()/rollback()
-# at once
 with pgsql_session.begin() as session:
     target_dt_obj = datetime.strptime(target_dt, "%Y-%m-%d")
     target_dt_str = target_dt_obj.strftime("%Y%m%d")
     target_dt_hyphen = target_dt_obj.strftime("%Y-%m-%d")
     next_dt_hyphen = (target_dt_obj + timedelta(days=1)).strftime("%Y-%m-%d")
 
-    query = f"CREATE TABLE IF NOT EXISTS morpheme_info_{target_dt_str} PARTITION OF morpheme_info FOR VALUES FROM ('{target_dt_hyphen}') TO ('{next_dt_hyphen}');"
-    print(query)
-    session.execute(query)
+    drop_table_query = f"DROP TABLE IF EXISTS morpheme_info_{target_dt_str};"
+    session.execute(drop_table_query)
+    create_table_query = f"CREATE TABLE IF NOT EXISTS morpheme_info_{target_dt_str} PARTITION OF morpheme_info FOR VALUES FROM ('{target_dt_hyphen}') TO ('{next_dt_hyphen}');"
+    session.execute(create_table_query)
 
     rows = []
     for h in s[0:hit_count]:
-        print(h.title, h.dt, h.url, h.content, h.replyList)
 
         # title 처리
         for word in okt.pos(h.title, norm=True):
@@ -98,5 +96,3 @@ with pgsql_session.begin() as session:
                     rows.append(row)
 
     session.add_all(rows)
-    # 어떤 포맷을 write 해야할 것인가
-# commits the transaction, closes the session
